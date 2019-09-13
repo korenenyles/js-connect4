2767f5ae0da61fd931e3b1008940e7c941309663
Ben Myers <benjmm@gmail.com>
2019-09-13 15:16:27 -0400

    friday afternoon progress

--- README.md
+++ README.md
@@ -4,19 +4,20 @@
 
     //Create discs in HTML/CSS
 
-//Create javascript DOM elements of grid & discs
+    //Create javascript DOM elements of grid & discs
 
     //Build click handler functions
 
-//function to add a disc to a column
--need to make it the right color
+    //function to add a disc to a column
+    -need to make it the right color
 
-//Method to alternate turns/colors
+    //Method to alternate turns/colors
 
-//way to control maximum number of discs allowed in column
+    //way to control maximum number of discs allowed in column
+
+
 
 //way to track current row & select particular child element
-
 
 //test for the win condition
 -test, column, row, diagonal
--- script.js
+++ script.js
@@ -15,33 +15,54 @@ let currentColumn = 'column1';
 
 //column click event listeners
 column1.addEventListener('click', function () {
-    clickColumn(column1); })
+    clickColumn(column1);
+})
 
 column2.addEventListener('click', function () {
-    clickColumn(column2);})
+    clickColumn(column2);
+})
 
 column3.addEventListener('click', function () {
-    clickColumn(column3);})
+    clickColumn(column3);
+})
 
 column4.addEventListener('click', function () {
-    clickColumn(column4);})
+    clickColumn(column4);
+})
 
 column5.addEventListener('click', function () {
-    clickColumn(column5);})
+    clickColumn(column5);
+})
 
 column6.addEventListener('click', function () {
-    clickColumn(column6);})
+    clickColumn(column6);
+})
 
 column7.addEventListener('click', function () {
-    clickColumn(column7);})
+    clickColumn(column7);
+})
 
 
 //main function to run when a column is clicked
 function clickColumn(column) {
     currentColumn = column;
-    console.log("clickColumn running; currentColumn=" + currentColumn.id);
-    createDisc();
-    //do all the turn things
+    if (currentColumn.childElementCount <= 5) {
+        console.log("clickColumn running; currentColumn=" + currentColumn.id);
+        createDisc();
+        console.log("type =" +currentColumn.childElementCount);
+        // console.log("whatDisk ="+whatDisc());
+        whatDisc();
+        //do all the turn things
+        //test for column win
+        //test for row win
+        //test for diagonal win
+        if (playerTurn === 'red') {
+            playerTurn = 'black';
+        } else { playerTurn = 'red' }
+        console.log("player turn set to " + playerTurn);
+    } else {
+        alert("Illegal Move!");
+    }
 }
 
 
@@ -49,6 +70,20 @@ function clickColumn(column) {
 function createDisc() {
     console.log("createDisc running");
     let newDisc = document.createElement('div');
-    newDisc.className = 'disc';
+    // newDisc.className = 'disc';
+    if (playerTurn === 'red') {
+        newDisc.className = 'discRed';;
+    } else { newDisc.className = 'discBlack'; }
     currentColumn.appendChild(newDisc);
+}
+
+function whatDisc(){
+    let columnArray = currentColumn.childNodes;
+    console.log("columnArray ="+columnArray);
+    console.log("columnArray[0] ="+columnArray[length-1].className);
+    // return(currentColumn.childElement[1].className);
+
+    // if (){
+    //     return 'red';
+    // } else {return 'black'}
 }

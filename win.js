5cbe112cd3d0067108fe3ba9232aa643cc6d0dd5
Joey <joeymizell1991@gmail.com>
2019-09-17 16:33:18 +0000

    Update win.js

--- win.js
+++ win.js
@@ -34,11 +34,33 @@ function winRow() {
     }
 }
 
+//its hard to refractor any of this considering we dont have a specific row-so im trying to figure out how we can use the array we set up and input the specific places within that array
 
-function winDiagonal() {
+//function winDiagonal() {
     // console.log("Running winDiagonal");
-}
+ //Connect Four•Check for win (diagonally - first direction):// Check for win diagonally, from top left
+// for(int row =0; row < board.length-3; row++){
+// for(int col =0; col < board[row].length-3; col++){
+     // if(board[row][col]!='□'&& board[row][col]== board[row+1][col+1]&& board[row][col]== board[row+2][col+2]&& board[row][col]==board[row+3][col+3]){
 
+    // return true;
+        //}
+    // }
+
+//}
+
+
+
+//second check direction
+// Check for win diagonally, from top right
+//for(int row =0; row < board.length-3; row++){
+    //for(int col =3; col < board[row].length; col++){
+        //if(board[row][col]!='□'&& board[row][col]== board[row+1][col-1]&& board[row][col]== board[row+2][col-2]&& board[row][col]== board[row+3][col-3]){
+            //return true;
+              //  }
+           //  }
+        // }
+          //  return 
 
 function winTie() {
     // console.log("Running winTie");
